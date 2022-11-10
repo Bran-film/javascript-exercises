28b157b096d8140b74d0fd941b3a1d295ba803ac
Bran-film <76977177+Bran-film@users.noreply.github.com>
2022-11-10 16:55:59 +0100

    Done JS 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,18 @@
-const ftoc = function() {
+// F -> C = xF = (x - 32) * 5/9C
+// C -> F = xC = (x * 9/5 + 32) F
 
+const ftoc = function (f) {
+  let c = ((f - 32) * 5) / 9;
+  return Number(Math.round(c + "e" + 1) + "e-" + 1);
 };
 
-const ctof = function() {
-
+const ctof = function (c) {
+  let f = (c * 9) / 5 + 32;
+  return Number(Math.round(f + "e" + 1) + "e-" + 1);
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
