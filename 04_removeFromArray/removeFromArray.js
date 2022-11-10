a08c28d63199e1d2ffc8f2fcd586dcf48979236f
Bran-film <76977177+Bran-film@users.noreply.github.com>
2022-11-10 15:43:33 +0100

    Done JS 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+  args.forEach((arg) => {
+    const index = array.indexOf(arg);
+    if (index > -1) array.splice(index, 1);
+  });
+  return array;
 };
+
+// removeFromArray([4, 5, 7, 9], 4);
 
 // Do not edit below this line
 module.exports = removeFromArray;
