2e97c5247cc2d7759e8eed860abbe260bd32d0e7
Bran-film <76977177+Bran-film@users.noreply.github.com>
2022-11-10 16:33:06 +0100

    Done JS 6 / Know I could be shorter

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  // year % 4 === 0 / true
+  // year % 100 === 0 / false
+  // year % 400 === 0 / true
 
+  if (year % 4 === 0) {
+    if (year % 400 === 0 || year % 100 !== 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
