874bb0fd96de7a71ec43db494cbfa415671b0e45
Bran-film <76977177+Bran-film@users.noreply.github.com>
2022-11-10 16:02:36 +0100

    Done JS 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let sum = 0;
+  let i = 0;
 
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  } else if (b > a) {
+    for (i = a; i <= b; i++) {
+      sum += i;
+    }
+    return sum;
+  } else {
+    for (i = b; i <= a; i++) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
