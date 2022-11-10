bb126f62d31a24cea8f599cb710dcd25ba1e88fa
Bran-film <76977177+Bran-film@users.noreply.github.com>
2022-11-10 13:22:25 +0100

    Done JS 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let newString = "";
 
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    for (i = 0; i < num; i++) {
+      newString += string;
+    }
+    return newString;
+  }
 };
+
+// repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
