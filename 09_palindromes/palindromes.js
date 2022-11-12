3f06a7c4573b062a26acf6592181c4c11bbd407c
Bran-film <76977177+Bran-film@users.noreply.github.com>
2022-11-12 12:14:24 +0100

    Done JS 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let check = /[\W_]/g;
+  var lowerCaseStr = str.toLowerCase().replace(check, "");
+  var reverseStr = lowerCaseStr.split("").reverse().join("");
+  return reverseStr === lowerCaseStr;
 };
 
 // Do not edit below this line
